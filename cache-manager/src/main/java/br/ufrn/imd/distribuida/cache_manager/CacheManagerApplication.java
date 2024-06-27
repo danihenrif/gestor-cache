c51a99b10859e0377c5dd3592705bec85a568416
Arturo Fonseca <arturofonsecas32@gmail.com>
2024-06-26 21:23:52 -0300

    Add java project

---
+++ cache-manager/src/main/java/br/ufrn/imd/distribuida/cache_manager/CacheManagerApplication.java
@@ -0,0 +1,13 @@
+package br.ufrn.imd.distribuida.cache_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacheManagerApplication.class, args);
+	}
+
+}
---
+++ cache-manager/src/test/java/br/ufrn/imd/distribuida/cache_manager/CacheManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufrn.imd.distribuida.cache_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
