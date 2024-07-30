5afb793181489136723cd2274dcf864cabe85679
danihenrif <danielhrqs4@yahoo.com>
2024-07-29 22:16:24 -0300

    adequacão para chamada da api de autenticação

--- cache-manager/src/main/java/br/ufrn/imd/distribuida/cache_manager/controller/DataCollectionController.java
+++ cache-manager/src/main/java/br/ufrn/imd/distribuida/cache_manager/controller/DataCollectionController.java
@@ -1,23 +1,32 @@
 package br.ufrn.imd.distribuida.cache_manager.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import br.ufrn.imd.distribuida.cache_manager.dto.DataCollectionDTO;
 import br.ufrn.imd.distribuida.cache_manager.exceptions.DataNotFoundException;
 import br.ufrn.imd.distribuida.cache_manager.exceptions.InvalidRequestException;
 import br.ufrn.imd.distribuida.cache_manager.model.DataCollection;
 import br.ufrn.imd.distribuida.cache_manager.service.DataCollectionService;
+import br.ufrn.imd.distribuida.cache_manager.service.TokenValidationService;
 import jakarta.validation.Valid;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1")
 public class DataCollectionController {
     private final DataCollectionService dataCollectionService;
+    private final TokenValidationService tokenValidationService;
 
-    DataCollectionController(DataCollectionService dataCollectionService) {
+    DataCollectionController(DataCollectionService dataCollectionService, TokenValidationService tokenValidationService) {
         this.dataCollectionService = dataCollectionService;
+        this.tokenValidationService = tokenValidationService;
     }
 
     @GetMapping("/cache/{id}")
@@ -25,8 +34,9 @@ public class DataCollectionController {
             @PathVariable("id") String id,
             @RequestHeader(value = "X-Application-Token", required = true) String token) {
 
-        if (!"dashboards:532452345345".equals(token)) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+        String appType = tokenValidationService.validateToken(token);
+        if (appType == null || !"dashboards".equals(appType)) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized application or invalid token.");
         }
 
         try {
@@ -44,7 +54,13 @@ public class DataCollectionController {
     }
 
     @PostMapping("/cache")
-    public ResponseEntity<DataCollectionDTO> save(@Valid @RequestBody DataCollection dataCollection) {
+    public ResponseEntity<DataCollectionDTO> save(@Valid @RequestBody DataCollection dataCollection,
+                                                  @RequestHeader(value = "X-Application-Token", required = true) String token) {
+        String appType = tokenValidationService.validateToken(token);
+        if (appType == null || !"data_access".equals(appType)) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+        }
+
         try {
             DataCollectionDTO dataCollectionDTO = this.dataCollectionService.create(dataCollection);
             return ResponseEntity.status(HttpStatus.CREATED).body(dataCollectionDTO);
---
+++ cache-manager/src/main/java/br/ufrn/imd/distribuida/cache_manager/service/TokenValidationService.java
@@ -0,0 +1,44 @@
+package br.ufrn.imd.distribuida.cache_manager.service;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class TokenValidationService {
+    private final RestTemplate restTemplate;
+
+    public TokenValidationService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public String validateToken(String token) {
+        String url = "http://10.7.40.177:8000/token/" + token;
+
+        // Configurar o cabeçalho da requisição
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Content-Type", "application/json");
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        try {
+            ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, entity, String.class);
+            if (response.getStatusCode() == HttpStatus.OK) {
+                String responseBody = response.getBody();
+                if (responseBody != null) {
+                    if (responseBody.contains("\"authenticated\": \"dashboards\"")) {
+                        return "dashboards";
+                    } else if (responseBody.contains("\"authenticated\": \"data_access\"")) {
+                        return "data_access";
+                    }
+                }
+            }
+            return null;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
